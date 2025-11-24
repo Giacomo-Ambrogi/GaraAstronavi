1229a3bda48a6bfef708618c557e5bf2d4ed6f06
Giacomo-Ambrogi <giacky597@gmail.com>
2025-11-24 13:06:56 +0100

    Loading generale

--- src/Astronave.java
+++ src/Astronave.java
@@ -1,2 +1,35 @@
-public class Astronave {
+import java.util.Scanner;
+
+public class Astronave extends Thread{
+    private String modello;
+    private int id;
+    private String nomePilota;
+    private String destinazione;
+    private Giudice g;
+
+    public Astronave (String modello, int id, String nomePilota, String destinazione, Giudice g) {
+        this.modello = modello;
+        this.id = id;
+        this.nomePilota = nomePilota;
+        this.destinazione = destinazione;
+        this.g = g;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Sono l'astronave: " + this.id + ", ed inizio la gara.");
+
+        gareggia();
+    }
+
+    private void gareggia() {
+        System.out.println("Inserisci il pianeta dove desideri andare:");
+
+        Scanner sc = new Scanner(System.in);
+        this.destinazione = sc.nextLine();
+
+        //g.verifica();
+
+        System.out.println("Ho scelto questa destinazione: " + this.destinazione);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Giudice g = new Giudice();
 
+        Astronave a1 = new Astronave("Apollo", 1, "Giacomo", "Marte", g);
     }
 }
